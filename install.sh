7601cdea0f0a783649b40daf30bc76d64301ed20
Yishu See <yishuyishu@gmail.com>
2023-02-10 17:11:23 +0800

    Add vim plug to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env zsh
 
 THISDIR="${0:a:h}"
+DOTVIM="${HOME}/.vim"
 vimrc="${HOME}/.vimrc"
+
+mkdir -p $DOTVIM
 
 if [ ! -L $vimrc ] && [ ! -f $vimrc ]
 then
   ln -s "$THISDIR"/.vimrc $vimrc
 fi
+
+PLUGGED="${DOTVIM}/plugged"
+AUTOLOAD="${DOTVIM}/autoload"
+
+mkdir -p "${PLUGGED}" "${AUTOLOAD}"
+
+ln -s "$THISDIR"/submodules/vim-plug/plug.vim "$AUTOLOAD"
