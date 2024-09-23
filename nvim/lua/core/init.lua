07ee1d9bc2284d585cba6916006d7f9b56f9d8ca
Yishu See <yishuyishu@gmail.com>
2024-09-23 13:17:14 +0800

    Add new keymaps and dressing

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -7,6 +7,10 @@ vim.opt.smartcase = true
 
 local map = vim.keymap.set
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+map("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+map("n", "<tab>", "<C-w>w", { desc = "Toggle active window" })
+map("i", "<C-e>", "<C-o>$", { desc = "Go to end of line" })
+map("i", "<C-h>", "<C-o>_", { desc = "Go to start of line" })
 
 require('core.lazy')
 require('core.which-key')
--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -96,6 +96,8 @@ require("lazy").setup({
           keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
         end,
       })
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
     end,
   },
   {
@@ -201,4 +203,35 @@ require("lazy").setup({
       })
     end,
   },
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      lint.linters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        javascriptreact = { "eslint_d" },
+        typescriptreact = { "eslint_d" }
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  }
 })
